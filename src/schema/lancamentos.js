006acdce0e02079012aaae6beb479443cf9be971
henriquenkt <henriquenkt@gmail.com>
2023-02-22 21:38:01 -0300

    modified:   src/schema/lancamentos.js

--- src/schema/lancamentos.js
+++ src/schema/lancamentos.js
@@ -1,23 +1,31 @@
 function mongoschema() {
   const mongoose = require("mongoose");
   var usuarioSchema = new mongoose.Schema({
-	codigo: {
+    codigo: {
       type: Number,
       require: true
     },
-    empresa: {
+    data: {
       type: String,
       require: true
     },
-    cnpj: {
+    descricao: {
       type: String,
       require: true
     },
-    valorConsultoria: {
+    tipo: {
       type: String,
       require: true
     },
-    valorGerenciamento: {
+    valor: {
+      type: String,
+      require: true
+    },
+    observacao: {
+      type: String,
+      require: true
+    },
+    creditoDebito: {
       type: String,
       require: true
     }
